4e3f9da441ad30f63daeae51e899ca256e1d2db6
Jagadish <Jagadishdevops.omega@gmail.com>
2023-12-21 19:18:31 +0530

    Shell

--- shellscr.sh
+++ shellscr.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+Curr_date=$(date '+%Y-%m-%d')
+
+
 echo "Hello, welcome to shell scripting"
-echo "Today date is $(date '+%Y-%m-%d')"
+echo "Today date is $(date)"
+echo "printing only date $Curr_date"
