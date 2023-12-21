49bb452ffc6e93d8b15446a7659a9d3657097111
Jagadish <Jagadishdevops.omega@gmail.com>
2023-12-21 19:23:14 +0530

    Shell

--- shellscr.sh
+++ shellscr.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 Curr_date=$(date '+%Y-%m-%d')
-
+year_r=$(date '+%d-%m-Y')
 
 echo "Hello, welcome to shell scripting"
 echo "Today date is $(date)"
 echo "printing only date $Curr_date"
+echo "Indian date format: $year_r"
